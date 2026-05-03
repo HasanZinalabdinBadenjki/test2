fd559e8496c7bf436ca79176c5d77cfb37844574
hasanBadenjki <Badnjkeh@gmail.com>
2026-05-03 18:10:03 +0300

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title: string = 'firstProject';
-  cars: any = ['BMW', 'Audi', new Date(), 'Toyota', true, 10, 'Chevrolet', 'Nissan', 'Volkswagen', 'Hyundai'];
+  isva:boolean=false;
+showAlert(e: any): void {
+  if (e.target.value.length > 7) {
+    this.isva = true;
+  } else {
+    this.isva = false;
+  }
 }
+
+
+}
+
